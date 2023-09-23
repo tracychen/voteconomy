fe4420f9ff05ec8d6ea263cca14b6e5c11b0cd5f
Tracy Chen <8510537+tracychen@users.noreply.github.com>
2023-09-23 17:37:11 -0400

    chore: remove starter ui package

--- services/apps/web/app/page.tsx
+++ services/apps/web/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "ui";
 import Home from "../components/Home";
 
 export default function Page() {
--- services/apps/web/next.config.js
+++ services/apps/web/next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["ui"],
   env: {
     BASE_GOERLI_PAYMASTER_URL: process.env.BASE_GOERLI_PAYMASTER_URL,
     POLYGON_MUMBAI_PAYMASTER_URL: process.env.POLYGON_MUMBAI_PAYMASTER_URL,
