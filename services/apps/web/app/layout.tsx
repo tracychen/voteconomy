52c2fbc71125843500b4e9ae7bb17519709b004e
Tracy Chen <8510537+tracychen@users.noreply.github.com>
2023-09-23 17:54:38 -0400

    chore: add tailwind configurations

--- services/apps/web/app/layout.tsx
+++ services/apps/web/app/layout.tsx
@@ -1,10 +1,19 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>{children}</body>
     </html>
   );
