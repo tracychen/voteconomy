67259d76409b39bb4ba31202a7cbfb8e9391d20c
Tracy Chen <8510537+tracychen@users.noreply.github.com>
2023-09-24 07:30:59 -0400

    chore: add additional networks to env file

--- services/apps/web/utils/env.ts
+++ services/apps/web/utils/env.ts
@@ -41,6 +41,24 @@ export const chainConfigs = {
     voteSessionValidationModuleContract:
       "0xe8b442ae8c9cc7a3ae5992da52d20767dfe8d825",
   },
+  // Scroll Sepolia Testnet
+  [534351]: {
+    voteContract: "0x0660b6c585eab822C9B4c8fbeFCC276DF17831EB",
+    rpcUrl: "https://scroll-testnet-public.unifra.io",
+    blockExplorerUrl: "https://scroll.l2scan.co",
+  },
+  // Gnosis Chiado Testnet
+  [10200]: {
+    voteContract: "0x831320aE215baD3A01A16C9CD5001A5c3988aED1",
+    rpcUrl: "https://rpc.chiadochain.net",
+    blockExplorerUrl: "https://gnosis-chiado.blockscout.com",
+  },
+  // Mantle testnet
+  [5001]: {
+    voteContract: "0x764A93978e1f0028e66cFE4DDDE097308dE2ED16",
+    rpcUrl: "https://rpc.testnet.mantle.xyz",
+    blockExplorerUrl: "https://explorer.testnet.mantle.xyz",
+  },
 };
 export const thirdwebSecretKey = process.env.THIRDWEB_SECRET_KEY || "";
 
